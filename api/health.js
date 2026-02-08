36c5b708cf46f39b5df36b9747f0c799ea923514
parsec202 <guus.derks@gmail.com>
2026-02-08 20:11:34 +0100

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,4 +1,4 @@
-export default async function handler(req: any, res: any) {
+export default async function handler(req, res) {
   const envCheck = {
     BRISE_RPC: !!process.env.BRISE_RPC,
     RELAYER_KEY: !!process.env.RELAYER_KEY,
